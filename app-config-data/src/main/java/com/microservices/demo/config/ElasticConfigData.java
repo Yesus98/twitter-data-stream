fd973f925cfe145d2431151a32be198baa5bf66b
Jesus Fernandez Garcia <jesus.fernandez-garcia@eviden.com>
2025-03-04 14:28:52 +0100

    Added elastic module

---
+++ app-config-data/src/main/java/com/microservices/demo/config/ElasticConfigData.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "elastic-config")
+public class ElasticConfigData {
+    private String indexName;
+    private String connectionUrl;
+    private int connectionTimeout;
+    private int socketTimeout;
+}
